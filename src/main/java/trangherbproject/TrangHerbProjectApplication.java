0f80217486e20bfb575c98452e8466fc06b00302
trungtinhhh <tinhtdtce180218@fpt.edu.vn>
2025-09-20 22:50:23 +0700

    Add TrangHerb Spring Boot project

---
+++ src/main/java/trangherbproject/TrangHerbProjectApplication.java
@@ -0,0 +1,13 @@
+package trangherbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrangHerbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrangHerbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/trangherbproject/TrangHerbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package trangherbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrangHerbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
